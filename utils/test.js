3947a60472a6527d7258cd0e5d28ab72443bee2d
corditus <obi.george.sims@gmail.com>
2025-11-19 00:46:17 +1100

    Add initial test script for server information logging

---
+++ utils/test.js
@@ -0,0 +1,15 @@
+/** @param {NS} ns */
+export async function main(ns) {
+console.log(`!!! Testing`)
+console.log(`server info`,{
+    server: ns.getServer(),
+})
+// let serverName;
+// let memorySize;
+
+// serverName = ns.args[0]
+// memorySize = ns.args[1]
+
+// const purchased = ns.purchaseServer(serverName, memorySize);
+// ns.tprint(purchased)
+}
